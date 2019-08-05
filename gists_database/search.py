ab3874f4986b473fdd1b22d4852b632d1000ffc7
Sandra Herrera <sandra.h.arriaga@gmail.com>
2019-08-05 16:07:04 +0000

    passed all test

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,21 @@
 from .models import Gist
+import datetime
 
-def search_gists(db_connection, **kwargs):
-    pass
+def search_gists(db_connection, github_id=None, created_at=None):
+
+    if github_id:
+        results = db_connection.execute('SELECT * FROM gists where github_id = ?', [github_id])
+
+    elif created_at:
+        results = db_connection.execute('SELECT * FROM gists where datetime(created_at) = datetime(?)', [created_at])
+    else:
+        results = db_connection.execute('SELECT * FROM gists')
+
+
+
+    gist_results = []
+    for item in results.fetchall():
+        gist_results.append(Gist(item))
+
+
+    return gist_results
